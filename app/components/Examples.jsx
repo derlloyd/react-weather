1a1345040126315d313f0f51c441973b4e7d9ed5
Derek Lloyd <derlloyd@outlook.com>
2016-11-09 11:22:41 -0500

    change examples page title

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,7 +10,10 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
-        <h3>Examples</h3>
+        <div>
+            <h3>Examples</h3>
+            <p>welcome to examples page</p>
+        </div>
     )
 };
 
