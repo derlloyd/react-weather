3438ba7861fe8776ba3ad5d9219428b1c843cb9b
Derek Lloyd <derlloyd@outlook.com>
2016-11-09 10:35:48 -0500

    prep for heroku

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ var express = require('express');
 
 // create app
 var app = express();
+const PORT = process.env.port || 3000
+
+// express middleware
+// openWeatherMap only works on http NOT https, so need to redirect all https to http
+app.use(function(req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        // call next when middleware done
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 // which folder to serve
 app.use(express.static('public'));
 
 // 2 arguments, port and function when server launches
-app.listen(3000, function() {
-    console.log('Express app running on port 3000')
+app.listen(PORT, function() {
+    console.log('Express app running on port ' + PORT)
 });
