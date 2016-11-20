78c826d8f2573652e4cfa30ddcdff49b69cc64c2
Derek Lloyd <dereklloyd@Dereks-MacBook-Pro.local>
2016-11-20 16:24:21 -0500

    Style about page and links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 const About = (props) => {
     return (
         <div>
-            <h3>About component</h3>
+            <h1 className="text-center">About component</h1>
             <p>welcome to the about page</p>
+            <p>Some tools used:</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a>
+                </li>The JS frameword
+                <li>    
+                    <a href="http://openweathermap.org">Open Weather Map</a>
+                </li>The search API
+            </ul>
         </div>
     );
 };
